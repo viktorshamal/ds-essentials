ec085c836e7f31981dcd1a13f7888544018d6bac
Viktor Shamal Andersen <sphinxominator@gmail.com>
2017-10-07 11:01:00 +0200

    Search and more equations

--- Home.js
+++ Home.js
@@ -9,29 +9,90 @@ if(!isNode) {
     new Clipboard('.equation')    
 }
 
-const Equation = ({id, title, latex}) => 
-    <div className='equation' data-clipboard-target={'#latex-' + id} key={id}>
-        <p>{title || id}</p>
-        <div dangerouslySetInnerHTML={{__html: katex.renderToString(latex)}}></div>
-        <span id={'latex-' + id}>{latex}</span>
-    </div>
+const EquationWrapper = styled.div`
+    padding: 0 2rem 1rem 2rem;
+    border: 1px solid;
+    margin: 1rem 0 1rem 0;
 
-const Home = ({ isLoading, posts }) =>
-    <Background>
-        <h1>Equations</h1>
-        {isLoading && "Loading..."}
-        {!isLoading && 
-            posts &&
-            posts.node &&
-            posts.node.list &&
-            posts.node.list.map(equation => <Equation {...equation}/>
-        )}
-    </Background>
+    .hidden {
+        opacity: 0;
+    }
+
+    &:hover {
+        background-color: grey;
+        cursor: pointer;
+    }
+`
+
+const Equation = ({id, title, latex, description}) => 
+    <EquationWrapper className='equation' data-clipboard-target={'#latex-' + id} key={id}>
+        <p>Title: <strong>{title || id}</strong></p>
+        <p>Description: {description}</p>
+        <div dangerouslySetInnerHTML={{__html: katex.renderToString(latex)}}></div>
+        <span className='hidden' id={'latex-' + id}>{latex}</span>
+    </EquationWrapper>
+
 
 const Background = styled.div`
     background-color: none;
 `
 
-export default createContainer(Home, () => ({
-    posts: query({ path: "equations" })
+const retriveListSafe = (isLoading, data) => {
+    return !isLoading && 
+        data &&
+        data.node &&
+        data.node.list
+}
+
+class EquationList extends React.Component {
+    constructor(props){
+        super(props)
+        const { isLoading, equations } = this.props
+
+        this.state = {
+            filteredEquations: null
+        }
+
+        const list = retriveListSafe(isLoading, equations)
+
+        this.applyFilter.bind(this)
+    }
+
+    applyFilter = (filter) => {
+        if(!filter) return this.setState({filteredEquations: null})            
+
+        const { isLoading, equations } = this.props
+
+        const safeEquations = retriveListSafe(isLoading, equations)
+
+        const filteredEquations = safeEquations && safeEquations.filter(equation => 
+           equation.title.includes(filter) ||
+           equation.description.includes(filter)
+        )
+        this.setState({filteredEquations})
+    }
+
+    handleInputChange = (event) => {
+        this.applyFilter(event.target.value)
+    }
+
+    render() {
+        const { isLoading, equations } = this.props
+        const { filteredEquations } = this.state
+
+        const list = filteredEquations || retriveListSafe(isLoading, equations)
+
+        return (
+            <Background>
+                <h1>Equations</h1>
+                <input type="text" onChange={this.handleInputChange}/>
+                {isLoading && "Loading..."}
+                {list && list.map(equation => <Equation {...equation}/>) }
+            </Background>
+        )
+    }
+}
+
+export default createContainer(EquationList, () => ({
+    equations: query({ path: "equations" })
 }));
---
+++ content/equations/cumulative-distribution-function.md
@@ -0,0 +1,5 @@
+---
+title: Cumulative distribution function
+latex: x^2 + 5 \cdot 10^5
+description: It's cummulative
+---
