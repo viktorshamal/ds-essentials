7c4e62150fcf5b0ae6547cbc801dd61c93cb9c1b
Viktor Shamal Andersen <sphinxominator@gmail.com>
2017-10-11 13:43:31 +0200

    Favicon

--- Html.js
+++ Html.js
@@ -12,6 +12,9 @@ export default ({ App, render }) => {
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.8.3/katex.min.css" integrity="sha384-B41nY7vEWuDrE9Mr+J2nBL0Liu+nl/rBXTdpQal730oTHdlrlXHzYMOhDU60cwde" crossorigin="anonymous"/>
             <meta charset="UTF-8" />
             <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <meta name="description" content="Essential equations for ITU DS students on 1st year." />
+            <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+            <link rel="icon" href="/favicon.ico" type="image/x-icon" />
         </head>
         <body>
           <Main />
