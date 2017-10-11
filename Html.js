a9abfaed9a6342a84b6ed31cecdb8e8df85ff7d7
Viktor Shamal Andersen <sphinxominator@gmail.com>
2017-10-11 13:50:59 +0200

    title fix

--- Html.js
+++ Html.js
@@ -12,7 +12,7 @@ export default ({ App, render }) => {
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.8.3/katex.min.css" integrity="sha384-B41nY7vEWuDrE9Mr+J2nBL0Liu+nl/rBXTdpQal730oTHdlrlXHzYMOhDU60cwde" crossorigin="anonymous"/>
             <meta charset="UTF-8" />
             <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-            <meta name="title" content="DS Essentials equations" />
+            <title>DS Essentials equations" <title/>
             <meta name="description" content="ITU DS students on 1st year." />
             <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
             <link rel="icon" href="/favicon.ico" type="image/x-icon" />
